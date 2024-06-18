3abf47e5cd736112fc8972044fb3262bed2591a9
ITR090 <raghad.4309@gmail.com>
2024-06-18 13:51:08 +0300

    app.js change for prod release

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const path = require('path')
 dotenv.config()
 const port = process.env.PORT || 5000
 // init application
 const app = express()
 // import user routers 
 const users_routers = require('./Routes/userRoutes')
+
+
+// check env if prod or dev
+
+if(process.env.NODE_ENV === 'Prod'){
+    const __dirname = path.resolve()
+    app.use(express.static(path.join(__dirname,'frontend/dist')))
+    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','dist','index.html')))
+}
 
 // DB connect
 mongoose.connect(process.env.MONGO_URI)
